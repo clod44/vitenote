46c793045be1799ce524ea9797f7dabd8676ccb3
clod44 <Rt_Sayochi@hotmail.com>
2025-02-06 03:07:28 +0300

    remove unused import

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -2,7 +2,7 @@ import GenericTopBar from "@/components/GenericTopBar"
 import Loading from "@/components/Loading"
 import Login from "@/components/Login"
 import { useAuth } from "@/hooks/useAuth"
-import { ActionIcon, Avatar, CopyButton, Stack, Text, TextInput, Tooltip } from "@mantine/core"
+import { ActionIcon, Avatar, CopyButton, Stack, TextInput, Tooltip } from "@mantine/core"
 import { IconCheck, IconCopy, IconLogout } from "@tabler/icons-react"
 
 const Profile = () => {
