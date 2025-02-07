7a94a8950fab0d0e9fc369b71c30ae196b5a5e67
clod44 <Rt_Sayochi@hotmail.com>
2025-02-07 21:01:39 +0300

    color scheme settings

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -1,13 +1,24 @@
 import GenericTopBar from "@/components/GenericTopBar"
-import { Text } from "@mantine/core"
-import { IconFishBone } from "@tabler/icons-react"
+import { Stack, Switch, useMantineColorScheme } from "@mantine/core"
+import { IconMoonStars, IconSun } from "@tabler/icons-react"
 
 const Settings = () => {
+    const { setColorScheme, colorScheme } = useMantineColorScheme({
+        keepTransitions: true,
+    });
     return (
         <>
             <GenericTopBar title="Settings" />
-            <IconFishBone size={32} className="mx-auto" />
-            <Text c={"dimmed"} size="xs" className="text-center">Empty</Text>
+            <Stack w={"100%"} align="center" gap={"md"} p={"md"}>
+                <Switch
+                    size="xl"
+                    color="dark.4"
+                    checked={colorScheme === 'dark'}
+                    onChange={(event) => setColorScheme(event.currentTarget.checked ? 'dark' : 'light')}
+                    onLabel={<IconSun size={16} stroke={2.5} />}
+                    offLabel={<IconMoonStars size={16} stroke={2.5} />}
+                />
+            </Stack>
         </>
     )
 }
